98b181a1aef3b20810ad983e149341245642cef5
ariffurmani <furmani.af.98@gmail.com>
2025-06-14 07:30:24 +0530

    Initial commit

---
+++ src/main/java/org/example/ParkingLotApp.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class ParkingLotApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Parking Lot Management System!");
+    }
+}
